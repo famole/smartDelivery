7d10463031d583ba676c6747d60b6235247e6e4d
fabian molina <famole@gmail.com>
2015-05-16 18:57:56 -0300

    config files 2

---
+++ frontend/config/main-local.php
@@ -0,0 +1,21 @@
+<?php
+
+$config = [
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => '1fT9Y0OOL7xsnI-kGfXND9uF4Cg5u4Ir',
+        ],
+    ],
+];
+
+if (!YII_ENV_TEST) {
+    // configuration adjustments for 'dev' environment
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = 'yii\debug\Module';
+
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = 'yii\gii\Module';
+}
+
+return $config;
