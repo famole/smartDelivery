da9ed6717d15718a0b8a88ed6349cfa935ab2ecd
rperez89 <rodrigopi2012@gmail.com>
2015-06-25 23:17:40 -0300

    zona

--- frontend/views/zona/_form.php
+++ frontend/views/zona/_form.php
@@ -17,12 +17,8 @@ use yii\widgets\ActiveForm;
         <script type="text/javascript" src="js/jquery.jcarousel.pack.js"></script>
         <script type="text/javascript" src="js/func.js"></script>
         
-        <script type="text/javascript" src="http://www.openlayers.org/api/OpenLayers.js"></script>
+      
 
-        <script type="text/javascript" src="js/OpenLayers_base.js"></script>
-
-
-        <script type="text/javascript" src="js/OpenLayers_features.js"></script>
         
         <style type="text/css">
         .smallmap {
@@ -30,7 +26,111 @@ use yii\widgets\ActiveForm;
         height: 300px;
         }
         </style>
-    </head>
+        
+<link rel="stylesheet" href="http://openlayers.org/en/v3.0.0/css/ol.css" type="text/css">
+<style>
+html              { width:100%; height:100%; margin:0; }
+body              { width:100%; height:100%; margin:0; font-family:sans-serif; }
+#map              { width:100%; height:100%; margin:0; }
+#wktwrapper       { position:absolute; display:block; bottom:20px; left:50px; right:50px; z-index:2000; }
+#wkt              { border:2px solid red; width:100%; }
+#toolbox          { position:absolute; top:8px; right:8px; padding:3px; border-radius:4px; color:#fff; background: rgba(255, 255, 255, 0.4); z-index:100; }
+#drawtoolswitcher { margin:0; padding:10px; border-radius:4px; background:rgba(0, 60, 136, 0.5); list-style-type:none; } 
+</style>
+</head>
+
+
+<body>
+
+
+
+<div id="wktwrapper">
+<input type="text" id="wkt" value="">    
+</div>
+
+<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+<script src="http://openlayers.org/en/v3.0.0/build/ol.js" type="text/javascript"></script>
+<script type="text/javascript">
+function addInteraction() {
+        // reset interaction:
+        if(typeof drawInteraction != 'undefined') map.removeInteraction(drawInteraction);
+	if(typeof selectInteraction != 'undefined') map.removeInteraction(selectInteraction);
+	if(typeof modifyInteraction != 'undefined') map.removeInteraction(modifyInteraction);
+        // get type:
+	var type = 'Polygon';
+	// Create a draw interaction and add it to the map:
+	drawInteraction = new ol.interaction.Draw({ source:source, type:type });
+	map.addInteraction(drawInteraction);
+	// Update geometry and change mode to modify after drawing is finished:
+	drawInteraction.on('drawend', function(e) {
+		var feature = e.feature; 
+		// remove draw interaction:
+		map.removeInteraction(drawInteraction);
+		// Create a select interaction and add it to the map:
+		selectInteraction = new ol.interaction.Select();
+		map.addInteraction(selectInteraction);
+		// select feature:
+		selectInteraction.getFeatures().push(feature);
+		// clone feature:
+		var featureClone = feature.clone();
+		// transform cloned feature to WGS84:
+		featureClone.getGeometry().transform('EPSG:3857', 'EPSG:4326');
+		// update WKT string:
+		document.getElementById("wkt").value = wkt.writeFeature(featureClone);
+		// Create a modify interaction and add to the map:
+		modifyInteraction = new ol.interaction.Modify({
+			features: selectInteraction.getFeatures()
+		});
+		map.addInteraction(modifyInteraction);  
+		// set listener to update geometry when feature is changed:
+		feature.on('change', function() {
+			// clone feature: 
+		        var featureClone = feature.clone();
+		        // transform cloned feature to WGS84:
+		        featureClone.getGeometry().transform('EPSG:3857', 'EPSG:4326');			
+			// set modified WKT string:
+			modifiedWKT = wkt.writeFeature(featureClone);
+			// set update trigger flag:
+			triggerUpdate=true; 
+		}); 
+	});
+}
+
+// update WKT on mouseup when geometry was modified:
+document.body.onmouseup = function() {
+	if(typeof modifiedWKT != 'undefined' && triggerUpdate)	{
+		// update WKT string:
+		document.getElementById("wkt").value = modifiedWKT;
+		// unset update trigger flag:
+		triggerUpdate=false;
+	}
+};
+
+var rasterLayer = new ol.layer.Tile({ source: new ol.source.MapQuest({layer: 'sat'}) });
+var source = new ol.source.Vector();
+var vectorLayer = new ol.layer.Vector({	source:source });
+var wkt = new ol.format.WKT();
+
+var map = new ol.Map({
+  target: 'map',
+  controls: ol.control.defaults().extend([ new ol.control.ScaleLine({ units:'metric' }) ]),
+  layers: [ rasterLayer, vectorLayer ],
+  view: new ol.View({
+    center: ol.proj.transform([0,10], 'EPSG:4326', 'EPSG:3857'),
+    zoom: 3
+  })
+});
+
+addInteraction();
+
+$("input[name=type]").change(function() { 
+	vectorLayer.getSource().clear();
+	document.getElementById("wkt").value = "";
+	addInteraction();
+});
+
+</script>
+ 
 <div class="zona-form">
 
     <?php $form = ActiveForm::begin(); ?>
@@ -53,44 +153,33 @@ use yii\widgets\ActiveForm;
 
 </div>
 
+ <div id="map"></div>
 
-
-        <body onload="iniciar();iniciarDrawFeacture();">
-            <div class="shell">
-              <div class="border">
-
-                <div class="slider">
-                  <ul>
-                    <li>
-                      <div class="item">
-                        <div id="mapa" class="smallmap">
-
-                        </div>
-
-                              </div>
-                    </li>
-
-                  </ul>
-                </div>
+                 
                <div id="main">
                   <div id="content" class="left">
                     <div class="highlight">
 
                     <table width="80%">
                             <tr>
-                                    <td align="center">
-                                    <input type="submit" name="crearFeature" id="crearFeature" value="Crear Zona" onclick="activarDrawFeacture()" /> 
-                                    </td>
+                                <td align="center">
+                                    <button type="button" id="crearFeature"  class="btn btn-primary" autocomplete="off" onclick="activarDrawFeacture()" >
+                                        Crear Zona
+                                    </button>    
+                                    
+                                </td>
                             <td align="center">
-                                    <input type="submit" name="editarFeature" id="editarFeature" value="Editar Zona" onclick="activarModifyFeacture()" />
+                                    <button type="button" id="editarFeature"  class="btn btn-primary" autocomplete="off" onclick="activarModifyFeacture()" >
+                                        Editar Zona
+                                    </button> 
+                                    
                             </td>
-                            <td align="center">
-                                    <input type="submit" name="mostrarFeature" id="mostrarFeature" value="Mostrar" onclick="mostrarArray()" /> 
-                                    </td>
-                                    </tr>
+                            
+                            </tr>
                     </table>
 
 
+                        
                     <p align="center"><strong>Coordenadas Zona</strong></p>
                     <div align="center">
                       <textarea name="coordenadasZona" cols="80" rows="10" id="coordenadasZona"></textarea>
