10c945638ecf8ccc55c102107097fcde0b73fec0
rperez89 <rodrigopi2012@gmail.com>
2015-07-18 19:24:06 -0300

    composer

--- frontend/controllers/DiaController.php
+++ frontend/controllers/DiaController.php
@@ -5,6 +5,8 @@ namespace frontend\controllers;
 use Yii;
 use yii\web\Controller;
 use frontend\models\zona;
+use frontend\models\Entrega;
+use frontend\models\Direccion;
 
 
 /**
@@ -18,6 +20,8 @@ class DiaController extends Controller{
     
      public function actionDia()
     {
+        $date = date("Y-m-d");
+         
         $zonas= array();
         
         $nullId = 0;
@@ -32,7 +36,24 @@ class DiaController extends Controller{
 
         }
         $zonasJson = json_encode($zonas); 
-        return $this->render('dia',['zonasJson'=>$zonasJson,]);
+        
+        $entregas = Entrega::find()
+        ->select('*')
+        ->where('ent_fecha = :date',[':date' => $date])
+        ->orderBy('ent_id')
+        ->all();
+        
+//        foreach($entregas as $entrega){
+//            
+//            $direccion = Direccion::find()            
+//            ->where('dir_id = :dir',[':dir' => $entrega.dir_id])           
+//            ->one();
+//            
+//            $item = array();
+//            
+//        }
+        
+        return $this->render('dia',['zonasJson'=>$zonasJson,'entregas'=>$entregas,]);
         
     }
     
